5d9e1b8c328544d7735f1b1eb49a5791f2f232e5
Adolfo Montilla <adolfo@kubi.cl>
2020-03-22 00:39:56 -0300

    Commit Inicial

---
+++ song-finder/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  song-finder
+//
+//  Created by Adolfo Montilla on 22-03-20.
+//  Copyright © 2020 Adolfo Montilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
